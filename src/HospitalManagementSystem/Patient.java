d9689a20940db6fa7afeb5b2c84b730b50c0d249
ganeshlonare <ganeshlonare311@gmail.com>
2024-02-26 21:03:30 +0530

    Completed viewPatient Functionality

--- src/HospitalManagementSystem/Patient.java
+++ src/HospitalManagementSystem/Patient.java
@@ -2,6 +2,7 @@ package HospitalManagementSystem;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.Scanner;
 
@@ -26,7 +27,7 @@ public class Patient {
             String query="Insert Into patients(name , age ,gender ) VALUES(?,?,?)";
             PreparedStatement preparedStatement=connection.prepareStatement(query);
             preparedStatement.setString(1,name);
-            preparedStatement.setInt(1,age);
+            preparedStatement.setInt(2,age);
             preparedStatement.setString(3,gender);
             int affectedRows=preparedStatement.executeUpdate();
             if(affectedRows>0){
@@ -37,6 +38,27 @@ public class Patient {
 
         }catch (SQLException e){
             e.printStackTrace();
+        }
+    }
+
+    public void viewPatients(){
+        String query="select * from patients";
+        try{
+            PreparedStatement preparedStatement=connection.prepareStatement(query);
+            ResultSet resultSet=preparedStatement.executeQuery();
+            System.out.println("+------------+---------------+-----------+-------------+");
+            System.out.println("| Patient ID |      Name     |    Age    |   Gender    |");
+            System.out.println("+------------+---------------+-----------+-------------+");
+            while(resultSet.next()){
+                int id=resultSet.getInt("id");
+                String name=resultSet.getString("name");
+                int age=resultSet.getInt("age");
+                String gender=resultSet.getString("gender");
+                System.out.printf("|%-12s|%-15s|%-11s|%-13s|");
+                System.out.println("+------------+---------------+-----------+-------------+");
+            }
+        }catch (SQLException e){
+            System.out.println(e.getMessage());
         }
     }
 }
