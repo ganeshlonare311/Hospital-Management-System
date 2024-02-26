2c4143ec5c7bd139c51c3da6514403ed545c80fe
ganeshlonare <ganeshlonare311@gmail.com>
2024-02-26 21:46:57 +0530

    Added check Doctor functionality

--- src/HospitalManagementSystem/Doctor.java
+++ src/HospitalManagementSystem/Doctor.java
@@ -4,15 +4,12 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.Scanner;
 
 public class Doctor {
     private Connection connection;
-    private Scanner scanner;
 
-    public Doctor(Connection connection , Scanner scanner){
+    public Doctor(Connection connection ){
         this.connection=connection;
-        this.scanner=scanner;
     }
 
     public void viewDoctor(){
@@ -34,5 +31,22 @@ public class Doctor {
         }catch (SQLException e){
             e.printStackTrace();
         }
+    }
+
+    public boolean getDoctorById(int id){
+        String query="SELECT * FROM doctors WHERE id = ?";
+        try{
+            PreparedStatement preparedStatement=connection.prepareStatement(query);
+            preparedStatement.setInt(1,id);
+            ResultSet resultSet=preparedStatement.executeQuery();
+            if(resultSet.next()){
+                return true;
+            }else{
+                return false;
+            }
+        }catch (SQLException e){
+            e.printStackTrace();
+        }
+        return false;
     }
 }
